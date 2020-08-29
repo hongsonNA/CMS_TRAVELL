8d1202abaa2dca1f32a550e63c7ae2dfa8cb2b44
hongsonNA <hson8711@gmail.com>
2020-08-29 10:30:47 +0700

    build API

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthControllerApi/AuthController@login');
+    Route::post('signup', 'AuthControllerApi/AuthController@signup');
+  
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'AuthControllerApi/AuthController@logout');
+        Route::get('user', 'AuthControllerApi/AuthController@user');
+    });
 });
